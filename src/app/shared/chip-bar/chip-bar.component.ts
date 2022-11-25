322ca99b614dd0dcb0991084e327d5b4b3ae3c85
Sumithdeepan A <sumithdeepan@gmail.com>
2022-11-25 08:51:54 +0530

    chip component added

---
+++ src/app/shared/chip-bar/chip-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chip-bar',
+  templateUrl: './chip-bar.component.html',
+  styleUrls: ['./chip-bar.component.scss'],
+})
+export class ChipBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -1,18 +1,20 @@
+import { ChipBarComponent } from './../chip-bar/chip-bar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 import { IonicModule } from '@ionic/angular';
+import { ListSearchComponent } from '../list-search/list-search.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, ListSearchComponent, ChipBarComponent],
   imports: [
     CommonModule,
     IonicModule,
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent, ListSearchComponent, ChipBarComponent
   ]
 })
 export class SharedModule { }
